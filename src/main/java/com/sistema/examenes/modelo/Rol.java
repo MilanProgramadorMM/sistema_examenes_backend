c3fac13197305e62bbbadec16bfa4c34a532b3b5
MilanProgramadorMM <137338471+MilanProgramadorMM@users.noreply.github.com>
2025-10-19 16:08:29 -0500

    firts commit

---
+++ src/main/java/com/sistema/examenes/modelo/Rol.java
@@ -0,0 +1,54 @@
+package com.sistema.examenes.modelo;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "rol_nombre")
+    private String rolNombre;
+
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "rol")
+    private Set<UsuarioRol> usuarioRoles = new HashSet<>();
+
+    public Rol(){
+
+    }
+
+    public Rol(Long rolId, String rolNombre) {
+        this.id = rolId;
+        this.rolNombre = rolNombre;
+    }
+
+    public Long getRolId() {
+        return id;
+    }
+
+    public void setRolId(Long rolId) {
+        this.id = rolId;
+    }
+
+    public String getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(String rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+
+    public Set<UsuarioRol> getUsuarioRoles() {
+        return usuarioRoles;
+    }
+
+    public void setUsuarioRoles(Set<UsuarioRol> usuarioRoles) {
+        this.usuarioRoles = usuarioRoles;
+    }
+}
---
+++ src/main/java/com/sistema/examenes/modelo/UsuarioRol.java
@@ -0,0 +1,51 @@
+package com.sistema.examenes.modelo;
+
+import javax.persistence.*;
+
+@Entity
+public class UsuarioRol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "rol_rol_id")
+    private Rol rol;
+
+    public UsuarioRol() {}
+
+    public UsuarioRol(Usuario usuario, Rol rol) {
+        this.usuario = usuario;
+        this.rol = rol;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+}
